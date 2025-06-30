4cf4df5eed5e7e97b1f960e60162f795a3844f1a
Raahim khan <raahimkhan18@gmail.com>
2025-06-30 16:50:00 +0530

    Added bottom navigation , marketplace & news
    Added bottom navigation , marketplace & news by Raahim

---
+++ app/src/main/java/com/example/lzycrazy/NewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lzycrazy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news) // ← layout from earlier
+    }
+}
