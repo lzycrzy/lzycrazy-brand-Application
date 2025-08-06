c0deb8b5784337b0f6ab22d5b4f8883ec10a0cfa
Kuldeep Tiwari <kuldeeptiwari9162@gmail.com>
2025-08-06 10:59:43 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "2.2.0"
-    id("kotlin-parcelize")
+    id("org.jetbrains.kotlin.android") version "2.1.21"
 }
 
 android {
     namespace = "com.example.lzycrazy"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lzycrazy"
@@ -29,12 +28,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 
     buildFeatures {
@@ -44,38 +43,35 @@ android {
 
 dependencies {
     // Core Android and Kotlin
-    implementation("androidx.core:core:1.16.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core:1.12.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.tbuonomo:dotsindicator:5.1.0")
 
-    // Retrofit and OkHttp
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
-    implementation("com.squareup.retrofit2:converter-gson:3.0.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.1.0")
+    // Retrofit and OkHttp (keep the latest version, not conflicting 2.x vs 3.x)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
     // Layout and UI
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("com.hbb20:ccp:2.7.3")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.hbb20:ccp:2.7.3")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
+
+    // AR and graphics
+    implementation("com.google.ar.sceneform:filament-android:1.17.1")
 
     // Activity and Navigation
     implementation("androidx.activity:activity:1.10.1")
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
-
-    // AR and graphics
-    //noinspection Aligned16KB
-    implementation("com.google.ar.sceneform:filament-android:1.17.1")
-    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.3.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
